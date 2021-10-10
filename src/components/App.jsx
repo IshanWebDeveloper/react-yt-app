398ebdd472aae08f69c33774b1222956702d675e
IshanWebDeveloper <ishananaguru@gmail.com>
2021-10-11 01:22:59 +0530

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import useVideos from '../hooks/useVideos';
+import SearchBar from './SearchBar';
+import VideoDetail from './VideoDetail';
+import VideoList from './VideoList';
+
+export default function App() {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos('aastha gill');
+
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList
+              videos={videos}
+              onVideoSelect={(video) => setSelectedVideo(video)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+export default function SearchBar({ onFormSubmit }) {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onFormSubmit(term);
+  };
+  return (
+    <div className=" search-bar ui segment ">
+      <form onSubmit={onSubmit} action="" className="ui form">
+        <div className="field">
+          <label htmlFor="video">Video Search</label>
+          <input
+            type="text"
+            name=""
+            id="video"
+            autoComplete="off"
+            onChange={(event) => setTerm(event.target.value)}
+            value={term}
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
